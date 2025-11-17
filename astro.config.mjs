c2a6fc79966e7d419fe82a6df4cfb6e0cd57e0bf
Julian-P-A <julian.jcpa@gmail.com>
2025-11-17 03:58:29 -0500

    Update dir ubications, and add img

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://Julian-P-A.github.io/WebsitePortfolio2025/',
-  base: '/WebsitePortfolio2025/', 
+  /*base: '/WebsitePortfolio2025/', solo cuando haga deploy*/
   vite: {
     plugins: [tailwindcss()]
   }
