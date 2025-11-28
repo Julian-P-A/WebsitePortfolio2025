3f9b7cf4aa1a7fa8debb308d8b309f4115f4a22d
Julian-P-A <julian.jcpa@gmail.com>
2025-11-28 13:42:53 -0500

    Change fonts from ttf to woff2, and testing image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  /*inlineStylesheets: 'auto',*/
   site: 'https://Julian-P-A.github.io/WebsitePortfolio2025/',
   base: '/WebsitePortfolio2025/',
   vite: {
