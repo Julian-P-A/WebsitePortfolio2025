89c9dc04f9bfb8d1592df33a6141412e2a53c4a2
Julian-P-A <julian.jcpa@gmail.com>
2025-11-28 13:17:09 -0500

    Test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  inlineStylesheets: 'auto',
+  /*inlineStylesheets: 'auto',*/
   site: 'https://Julian-P-A.github.io/WebsitePortfolio2025/',
   base: '/WebsitePortfolio2025/',
   vite: {
