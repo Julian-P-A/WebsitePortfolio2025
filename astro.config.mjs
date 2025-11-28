515cfd1e95ef6565c0b367692ba0081c1ff84f6c
Julian-P-A <julian.jcpa@gmail.com>
2025-11-28 12:23:40 -0500

    Fixed optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   inlineStylesheets: 'auto',
   site: 'https://Julian-P-A.github.io/WebsitePortfolio2025/',
-  /*base: '/WebsitePortfolio2025/',*/
+  base: '/WebsitePortfolio2025/',
   vite: {
     plugins: [tailwindcss()]
   }
