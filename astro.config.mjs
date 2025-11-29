b4fe939432d3f36c8e05f23e8ce59ec8a101ead5
Julian-P-A <julian.jcpa@gmail.com>
2025-11-29 04:10:45 -0500

    Change astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+const isVercel = process.env.VERCEL === '1';
+
 export default defineConfig({
   site: 'https://Julian-P-A.github.io/WebsitePortfolio2025/',
 
-  base: '/WebsitePortfolio2025/',
+  base: isVercel ? '/' : '/WebsitePortfolio2025/',
   vite: {
     plugins: [tailwindcss()]
   },
