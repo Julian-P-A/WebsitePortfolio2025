6423c00e74273a99cce46431c0ca286e17ae279e
Julian-P-A <julian.jcpa@gmail.com>
2025-11-29 03:07:41 -0500

    Optimize icons size, change the hero, add viewport animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://Julian-P-A.github.io/WebsitePortfolio2025/',
+
   base: '/WebsitePortfolio2025/',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [react()]
 });
